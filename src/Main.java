462610cf5ddd40d5b62c12bc29327c2091f0ed7e
Rafael Riki Ogawa Osiro <rafaelosiro@hotmail.com>
2022-08-13 01:40:52 -0300

    Created overtime calculation and salary calculation

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,23 @@
-
+import java.util.Scanner;
 import ViewModel.ViewModel;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
+		int hour;
+		float salary;
+		
+		Scanner scan = new Scanner(System.in);
+		ViewModel viewModel = new ViewModel();
+		
+		System.out.println("Digite a quantidade de horas total trabalhadas: ");
+		hour = scan.nextInt();
+		
+		System.out.println("Digite o valor da hora trabalhada: ");
+		salary = scan.nextFloat();
+		
+		System.out.println("O salario é: R$" + viewModel.funcCalculateSalary(hour, salary));
 	}
 
 }
