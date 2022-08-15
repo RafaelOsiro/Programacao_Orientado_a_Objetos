0cd61b9fe0c0ab5a5265af1f69df817a5005e99a
Rafael Riki Ogawa Osiro <rafaelosiro@hotmail.com>
2022-08-14 21:59:45 -0300

    Created RetirementViewModel, EmployeeModel and MenuView

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,70 @@
+import java.util.Scanner;
+
+import View.MenuView;
+import ViewModel.RetirementViewModel;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		RetirementViewModel retirementVM = new RetirementViewModel();
+		MenuView menuView = new MenuView();
+		
+		retirementVM.setMenuView(menuView);
+		
+		int option = 0;
+		boolean isExit = false;
+		boolean isValid = false;
+		
+		@SuppressWarnings("resource")
+		Scanner scan = new Scanner(System.in);
+		
+		do {
+			System.out.println("Sistema para calcular a aposentadoria de um funcionario");
+			menuView.funcPrintMenu();
+			
+			option = scan.nextInt();
+			
+			switch (option) {
+				case 1: {
+					retirementVM.funcCreateNewEmployee();
+					break;
+				}
+				case 2: {
+					retirementVM.funcSearchEmployee();
+					break;
+				}
+				case 3: {
+					retirementVM.funcCheckRetirement();
+					break;
+				} 
+				case 4: {
+					
+					do {
+						menuView.funcPrintExit();
+						option = scan.nextInt();
+						
+						if (option == 1) {
+							isExit = true;
+							isValid = true;
+						} else if (option == 2) {
+							isExit = false;
+							isValid = true;
+						} else {
+							System.out.println("Escolha uma opcao valida");
+							isValid = false;
+						}
+						
+					} while (isValid == false);
+					break;
+				}
+				default: {
+					break;
+				}
+			}
+			
+		} while (isExit == false);
+		
+		System.out.println("O sistema foi encarrado com sucesso!");
 	}
-
 }
