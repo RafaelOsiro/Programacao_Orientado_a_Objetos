a21cd7b99cf1d36ae15b9975b453b6275a9234af
Rafael Riki Ogawa Osiro <rafaelosiro@hotmail.com>
2022-09-19 22:39:49 -0300

    feature: Heart Rates
    Heart Rates View
    Heart Rates View Model
    Heart Rates Model

--- src/Model/HeartRatesModel.java
+++ src/Model/HeartRatesModel.java
@@ -1,13 +1,34 @@
 package Model;
 
-import java.util.Date;
+import java.time.LocalDate;
 
 public class HeartRatesModel {
 	
-	String firstName;
-	String lastName;
-	Date birthDate;
+	private String firstName;
+	private String lastName;
+	private LocalDate birthday;
 	
+	public String getFirstName() {
+		return firstName;
+	}
 	
-
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	
+	public String getLastName() {
+		return lastName;
+	}
+	
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	public LocalDate getBirthday() {
+		return birthday;
+	}
+	
+	public void setBirthday(LocalDate birthday) {
+		this.birthday = birthday;
+	}
 }
