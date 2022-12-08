bf09db09c3c79e15c3e568e517e82e63682fa0ce
Rafael Riki Ogawa Osiro <rafaelosiro@hotmail.com>
2022-12-08 01:35:54 -0300

    First commit of the project

---
+++ src/main/java/io/ucb/rafael/bluefoodpay/PaymentResponse.java
@@ -0,0 +1,23 @@
+package io.ucb.rafael.bluefoodpay;
+
+public class PaymentResponse {
+
+	private StatusPagamento status;
+	private String error;
+	
+	public PaymentResponse(StatusPagamento status) {
+		this.status = status;
+	}
+	
+	public PaymentResponse(String error) {
+		this.error = error;
+	}
+	
+	public StatusPagamento getStatus() {
+		return status;
+	}
+	
+	public String getError() {
+		return error;
+	}
+}
---
+++ src/test/java/io/ucb/rafael/bluefoodpay/BlueFoodPayApplicationTests.java
@@ -0,0 +1,13 @@
+package io.ucb.rafael.bluefoodpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlueFoodPayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
